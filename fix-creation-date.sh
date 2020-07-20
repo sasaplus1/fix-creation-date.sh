24f11630c76072952a4bd487ae97fec0682d1345
sasaplus1 <sasaplus1@gmail.com>
2020-07-20 18:17:11 +0900

    simplify

--- fix-creation-date.sh
+++ fix-creation-date.sh
@@ -22,9 +22,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-set -e
-set -u
-set -o pipefail
+set -euo pipefail
 
 trap 'printf -- "%s\n" "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
